4580df633a82addb65f3bb882734aa575362cfd4
andip71 <andreasp@gmx.de>
2013-09-11 11:36:04 +0200

    Boeffla ramfs: Remove config file support (this is now obsolete)

--- ramdisk_boeffla/sbin/boeffla-init.sh
+++ ramdisk_boeffla/sbin/boeffla-init.sh
@@ -7,32 +7,19 @@ SYSTEM_DEVICE="/dev/block/mmcblk0p9"
 CACHE_DEVICE="/dev/block/mmcblk0p8"
 DATA_DEVICE="/dev/block/mmcblk0p12"
 
+
 # define file paths, depending on whether we are on Samsung or CM
 if [ -d "/lib/modules" ] ; then
 	# Samsung
-	BOEFFLA_PATH="/data/media/boeffla-kernel"
 	BOEFFLA_DATA_PATH="/data/media/boeffla-kernel-data"
 	KERNEL="SAM"
 else
 	# CM
-	BOEFFLA_PATH="/data/media/0/boeffla-kernel"
 	BOEFFLA_DATA_PATH="/data/media/0/boeffla-kernel-data"
 	KERNEL="CM"
 fi
-
-BOEFFLA_CONFIG="$BOEFFLA_PATH/boeffla-kernel.conf"
 BOEFFLA_LOGFILE="$BOEFFLA_DATA_PATH/boeffla-kernel.log"
 
-
-# check if Boeffla kernel path exists and if so, execute config file
-if [ -d "$BOEFFLA_PATH" ] ; then
-
-	# check and create or update the configuration file
-	. /sbin/boeffla-configfile.inc
-
-else
-	BOEFFLA_CONFIG=""
-fi
 
 # If not yet exists, create a boeffla-kernel-data folder on sdcard 
 # which is used for many purposes (set permissions and owners correctly)
@@ -48,18 +35,22 @@ mv $BOEFFLA_LOGFILE.2 $BOEFFLA_LOGFILE.3
 mv $BOEFFLA_LOGFILE.1 $BOEFFLA_LOGFILE.2
 mv $BOEFFLA_LOGFILE $BOEFFLA_LOGFILE.1
 
+
 # Initialize the log file (chmod to make it readable also via /sdcard link)
 echo $(date) Boeffla-Kernel initialisation started > $BOEFFLA_LOGFILE
 /sbin/busybox chmod 666 $BOEFFLA_LOGFILE
 /sbin/busybox cat /proc/version >> $BOEFFLA_LOGFILE
 echo "=========================" >> $BOEFFLA_LOGFILE
 
+
 # Include version information about firmware in log file
 /sbin/busybox grep ro.build.version /system/build.prop >> $BOEFFLA_LOGFILE
 echo "=========================" >> $BOEFFLA_LOGFILE
 
+
 # Fix potential issues and clean up
 . /sbin/boeffla-fix-and-cleanup.inc
+
 
 # Custom boot animation support only for Samsung Kernel,
 # boeffla sound change delay changed only for Samsung Kernel
@@ -72,71 +63,47 @@ fi
 # Then set the options which change the stock kernel defaults
 # to Boeffla-Kernel defaults
 
-# Ext4 tweaks
-. /sbin/boeffla-ext4.inc
+# Ext4 tweaks default to on
+sync
+/sbin/busybox mount -o remount,commit=20,noatime $CACHE_DEVICE /cache
+sync
+/sbin/busybox mount -o remount,commit=20,noatime $DATA_DEVICE /data
+sync
+echo $(date) Ext4 tweaks applied >> $BOEFFLA_LOGFILE
 
-# Sdcard buffer tweaks
-. /sbin/boeffla-sdcard.inc
+# Sdcard buffer tweaks default to 256 kb
+echo 256 > /sys/block/mmcblk0/bdi/read_ahead_kb
+echo $(date) "SDcard buffer tweaks (256 kb) applied for internal sd memory" >> $BOEFFLA_LOGFILE
+echo 256 > /sys/block/mmcblk1/bdi/read_ahead_kb
+echo $(date) "SDcard buffer tweaks (256 kb) applied for external sd memory" >> $BOEFFLA_LOGFILE
 
-# AC and USB charging rate
-. /sbin/boeffla-chargingrate.inc
+# AC and USB charging rate defaults default to 1100 and 475 mA
+echo "1100" > /sys/kernel/charge_levels/charge_level_ac
+echo $(date) "AC charge rate set to 1100 mA" >> $BOEFFLA_LOGFILE
+echo "475" > /sys/kernel/charge_levels/charge_level_usb
+echo $(date) "USB charge rate set to 475 mA" >> $BOEFFLA_LOGFILE
 
-# Android logger
-. /sbin/boeffla-androidlogger.inc
+# Android logger defaults to off
+echo 0 > /sys/kernel/logger_mode/logger_mode
+echo $(date) Android logger disabled >> $BOEFFLA_LOGFILE
 
-# Kernel logger
-. /sbin/boeffla-kernellogger.inc
-
+# Kernel logger defaults to off (only Samsung kernels)
+if [ "SAM" == "$KERNEL" ]; then
+	echo 0 > /sys/kernel/printk_mode/printk_mode
+	echo $(date) Kernel logger disabled >> $BOEFFLA_LOGFILE
+fi
 
 # Now wait for the rom to finish booting up
-# (by checking for any android process)
+# (by checking for the android acore process)
 while ! /sbin/busybox pgrep android.process.acore ; do
   /sbin/busybox sleep 1
 done
-echo $(date) Rom boot trigger detected, continuing after 8 more seconds... >> $BOEFFLA_LOGFILE
-/sbin/busybox sleep 8
+echo $(date) Rom boot trigger detected, continuing after 12 more seconds... >> $BOEFFLA_LOGFILE
+/sbin/busybox sleep 12
 
-# Governor
-. /sbin/boeffla-governor.inc
 
-# IO Scheduler
-. /sbin/boeffla-scheduler.inc
-
-# CPU max frequency
-. /sbin/boeffla-cpumaxfrequency.inc
-
-# CPU undervolting support
-. /sbin/boeffla-cpuundervolt.inc
-
-# GPU frequency
-. /sbin/boeffla-gpufrequency.inc
-
-# GPU undervolting support
-. /sbin/boeffla-gpuundervolt.inc
-
-# LED settings
-. /sbin/boeffla-led.inc
-
-# Touch boost switch / Touch wake
-. /sbin/boeffla-touchboost-touchwake.inc
-
-# MDNIE settings (sharpness fix)
-. /sbin/boeffla-mdnie.inc
-
-# System tweaks
-. /sbin/boeffla-systemtweaks.inc
-
-# zRam support
-. /sbin/boeffla-zram.inc
-
-# Support for additional network protocols (CIFS, NFS)
-. /sbin/boeffla-network.inc
-
-# Support for xbox controller
-. /sbin/boeffla-xbox.inc
-
-# exFat support
-. /sbin/boeffla-exfat.inc
+# Configuration app support (delete the old scriptmanager app)
+. /sbin/boeffla-app.inc
 
 # Turn off debugging for certain modules
 . /sbin/boeffla-disabledebugging.inc
@@ -144,15 +111,8 @@ echo $(date) Rom boot trigger detected, continuing after 8 more seconds... >> $B
 # Auto root support
 . /sbin/boeffla-autoroot.inc
 
-# Configuration app support
-. /sbin/boeffla-app.inc
-
 # EFS backup
 . /sbin/boeffla-efsbackup.inc
-
-# Boeffla Sound (wait another 4 seconds first to cover for slow rom loading)
-/sbin/busybox sleep 4
-. /sbin/boeffla-sound.inc
 
 # init.d support
 . /sbin/boeffla-initd.inc
