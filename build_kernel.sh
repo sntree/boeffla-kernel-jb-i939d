f6a00dc1f170eafe3011ac3c2e54bf38f747548d
sn <sntree@gmail.com>
2013-11-07 12:54:13 +0800

    make build_kernel.sh more easy to build version

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
 
+KERNEL_BRANCH=Boeffla-Kernel
+KERNEL_VERSION=3.2
+
 export ARCH=arm
-export KBUILD_BUILD_USER=Boeffla-Kernel-3.2-20131107
+export KBUILD_BUILD_USER="-"$KERNEL_BRANCH"-"$KERNEL_VERSION"-"`(date +%Y%m%d)`
 export KBUILD_BUILD_HOST=sntree
+
+echo "Will build kernel " $KBUILD_BUILD_USER"@"$KBUILD_BUILD_HOST
 
 GOOGLE_TC=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
 LINARO_TC=/home/guni/kernel/arm-eabi-linaro-4.8.2/bin/arm-eabi-
-
-KERNELDIR=`readlink -f .`
-BOEFFLA_RAMDISK="ramdisk_boeffla"
-RAMDISK_PATH="ramdisk_samsung"
-RAMDISK_NAME="boot.img-ramdisk"
-ZIMAGE="arch/arm/boot/zImage"
-MODULES="modules"
-TMP_PATH="tmp"
-MKBOOT_PATH="mkboot"
-OUTPUT_PATH="output"
-CWM_PATH="cwm"
-BASE_ADDR="0x10000000"
-RAMDISK_ADDR="0x11000000"
-KERNEL_VERSION="3.1stable"
-
-RMV_MODULES=(commkm.ko mvpkm.ko pvtcpkm.ko)
 
 print_error()
 {
@@ -183,129 +172,7 @@ DEFCONFIG=$OP"_00_defconfig"
 make $DEFCONFIG
 make -j4
 
-exit 0
 
 check_zimage
 
-# Clear tmp directory
-rm -rf $MODULES
-rm -rf $TMP_PATH
-mkdir $MODULES
-mkdir $TMP_PATH
-
-# Copy all compiled kernel modules into modules directory
-find . -name "*.ko" | xargs cp -t $MODULES
-
-# Uncompress ramdisk image into tmp directory
-RAMDISK=$RAMDISK_PATH"/boot.img-ramdisk_"$OP".gz"
-
-cd $TMP_PATH
-gunzip -c $KERNEL_
-tar xvfz $RAMDISK -C $TMP_PATH"/"$OP > /dev/null
-
-TMP_RAMDISK=$TMP_PATH"/"$OP
-if [ -d $TMP_RAMDISK ]
-then
-    echo "Done uncompressing "$OP" ramdisk!"
-fi
-
-# Remove and copy new boeffla specific configuration files into corresponding directory
-rm -rf $TMP_RAMDISK"/res"
-cp -R $BOEFFLA_RAMDISK"/res" $TMP_RAMDISK
-rm -rf $TMP_RAMDISK"/sbin/boeffla-*"
-rm -rf $TMP_RAMDISK"/sbin/busybox"
-
-for f in $BOEFFLA_RAMDISK"/sbin/"*
-do
-    cp $f $TMP_RAMDISK"/sbin/"
-done
-
-for f in $TMP_RAMDISK"/sbin/"*
-do
-    chmod 750 $f
-done
-
-exit 0
-
-# Search and replace and add boeffla activation script into init
-
-# Copy zImage and kernel modules and remove unnecessary modules
-mv $TMP_RAMDISK $TMP_PATH"/"$RAMDISK_NAME  # rename ramdisk folder
-
-check_zimage
-echo Copy zImage from build path to tmp
-cp $ZIMAGE $TMP_PATH
-
-check_modules
-echo Copy all kernel modules from build path to tmp
-rm -rf $TMP_PATH"/"$RAMDISK_NAME"/lib/modules"
-cp -R $MODULES $TMP_PATH"/"$RAMDISK_NAME"/lib"
-echo Remove some unnecessary modules from tmp
-for idx in "${RMV_MODULES[@]}"
-do
-    rm -rf $TMP_PATH"/"$RAMDISK_NAME"/lib/modules/"$idx
-done
-
-check_kernel $TMP_PATH
-
-# Build mkboot executable binary from source
-build_mkboot $MKBOOT_PATH $TMP_PATH
-
-# Build the kernel into boot.img
-cd $TMP_PATH
-echo "Creating ramdisk cpio archive ..."
-./mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
-
-echo "Building boot.img ..."
-
-ramdisk_params=""
-if [ "$RAMDISK_ADDR" != "" ]
-then
-  ramdisk_params="--ramdiskaddr $RAMDISK_ADDR"
-fi
-
-./mkbootimg --kernel zImage --ramdisk ramdisk.gz -o boot.img --base $BASE_ADDR $ramdisk_params
-
-cd ..
-
-# Move built boot.img into output directory and rename to corresponding op
-
-if [ -d $OUTPUT_PATH"/"$TC ]
-then
-    echo "Found output directory!"
-    if [ -e $OUTPUT_PATH"/"$TC"/boot_"$OP".img" ]
-    then
-        rm -rf $OUTPUT_PATH"/"$TC"/boot_"$OP".img"
-    fi
-
-    if [ -e $OUTPUT_PATH"/"$TC"/boeffla_kernel_"$KERNEL_VERSION"_"$OP".zip" ]
-    then
-        rm -rf $OUTPUT_PATH"/"$TC"/boeffla_kernel_"$KERNEL_VERSION"_"$OP".zip"
-    fi
-else
-    mkdir -p $OUTPUT_PATH"/"$TC
-fi
-
-if [ -e $TMP_PATH"/boot.img" ]
-then
-    echo
-    echo "boot.img created"
-    echo "Moving boot.img to output directory..."
-    rm -rf $OUTPUT_PATH"/"$TC"/boot_"$OP".img"
-    mv $TMP_PATH"/boot.img" $OUTPUT_PATH"/"$TC"/boot_"$OP".img"
-fi
-
-# Pack boot.img as CWM flashable zip file and move to output directory
-
-`cp $OUTPUT_PATH"/"$TC"/boot_"$OP".img" $CWM_PATH"/boot.img"`
-cd $CWM_PATH
-echo `zip -r -9 "../"$OUTPUT_PATH"/"$TC"/boeffla_kernel_"$KERNEL_VERSION"_"$OP".zip" * > /dev/null`
-cd ..
-
-# Clean all temporary files...
-rm -rf $TMP_PATH
-rm -rf $CWM_PATH"/boot.img"
-rm -rf $MODULES
-rm -rf $MKBOOT_PATH"/mkbootfs"
-rm -rf $MKBOOT_PATH"/mkbootimg"
 
